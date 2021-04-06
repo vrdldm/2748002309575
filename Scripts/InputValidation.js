92f50846a39b00726aad0bf91ec3d5e5e843bd59
vrdldm <82046810+vrdldm@users.noreply.github.com>
2021-04-06 22:37:59 +0530

    Add files via upload

---
+++ Scripts/InputValidation.js
@@ -0,0 +1,1 @@
+No Content: https://liskernel01.attunelive.net/QRCode/Scripts/InputValidation.js
---
+++ Scripts/LoginJS/owl.carousel.js
@@ -0,0 +1,1 @@
+No Content: https://liskernel01.attunelive.net/QRCode/Scripts/LoginJS/owl.carousel.js
